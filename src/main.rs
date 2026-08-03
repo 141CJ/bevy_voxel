e31197b19e480ae7599e193efb7e59db435e9eff
141CJ <141cj+prod@proton.me>
2026-08-03 18:47:02 -0400

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,62 @@
+use bevy::{
+    dev_tools::fps_overlay::{FpsOverlayConfig, FpsOverlayPlugin},
+    prelude::*,
+};
+
+fn main() {
+    App::new()
+        .add_plugins((
+            DefaultPlugins,
+            FpsOverlayPlugin {
+                config: FpsOverlayConfig {
+                    text_config: TextFont {
+                        font_size: FontSize::Px(42.0),
+                        ..default()
+                    },
+                    frame_time_graph_config: bevy::dev_tools::fps_overlay::FrameTimeGraphConfig {
+                        enabled: true,
+                        min_fps: 100.,
+                        target_fps: 144.,
+                    },
+                    ..default()
+                },
+            },
+        ))
+        .add_systems(Startup, scene.spawn())
+        .add_systems(Update, overlay_config)
+        .run();
+}
+
+fn scene() -> impl SceneList {
+    bsn_list! [
+        (
+            #CircularBase
+            Mesh3d(asset_value(Circle::new(4.0)))
+            MeshMaterial3d::<StandardMaterial>(asset_value(Color::WHITE))
+            Transform::from_rotation(Quat::from_rotation_x(-std::f32::consts::FRAC_PI_2))
+        ),
+        (
+            #Cube
+            Mesh3d(asset_value(Cuboid::new(1.0, 1.0, 1.0)))
+            MeshMaterial3d::<StandardMaterial>(asset_value(Color::srgb_u8(124, 144, 255)))
+            Transform::from_xyz(0.0, 0.5, 0.0)
+        ),
+        (
+            PointLight {
+                shadow_maps_enabled: true,
+            }
+            Transform::from_xyz(4.0, 8.0, 4.0)
+        ),
+        (
+            Camera3d
+            template_value(Transform::from_xyz(-2.5, 4.5, 9.0).looking_at(Vec3::ZERO, Vec3::Y))
+        )
+    ]
+}
+
+fn overlay_config(input: Res<ButtonInput<KeyCode>>, mut overlay: ResMut<FpsOverlayConfig>) {
+    if input.just_pressed(KeyCode::F1) {
+        overlay.enabled = !overlay.enabled;
+        overlay.frame_time_graph_config.enabled = !overlay.frame_time_graph_config.enabled;
+    }
+}
