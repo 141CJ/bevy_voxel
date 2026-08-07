2987699baa6b29a33dad2f3954971559bc026b1a
141CJ <141cj+prod@proton.me>
2026-08-07 16:38:11 -0400

    refactor: refactor chunks to follow bevy's ECS paradigm

---
+++ src/chunk.rs
@@ -0,0 +1,117 @@
+use bevy::{
+    asset::Assets,
+    color::Color,
+    ecs::{
+        component::Component,
+        entity::Entity,
+        query::Without,
+        resource::Resource,
+        system::{Commands, Query, ResMut},
+    },
+    math::primitives::Cuboid,
+    mesh::{Mesh, Mesh3d},
+    pbr::{MeshMaterial3d, StandardMaterial},
+    transform::components::Transform,
+};
+use ndarray::Array3;
+use noise::{NoiseFn, Perlin};
+
+#[derive(Clone, Copy, PartialEq, Debug)]
+enum BlockType {
+    Air,
+    Grass,
+    Stone,
+}
+
+#[derive(Component)]
+pub struct Chunk {
+    length: usize,
+    height: usize,
+    blocks: Array3<BlockType>,
+    scale: f64, // Noise scale
+    amp: f64,   // Noise amplitude
+    noise: Perlin,
+}
+
+#[derive(Resource)]
+pub struct ChunkGenerated;
+
+#[derive(Component)]
+pub struct ChunkRendered;
+
+impl Chunk {
+    pub fn new(length: usize, height: usize, seed: u32) -> Self {
+        Chunk {
+            blocks: Array3::from_elem((length, height, length), BlockType::Air),
+            scale: 32.,
+            amp: 32.,
+            noise: Perlin::new(seed),
+            length,
+            height,
+        }
+    }
+
+    fn generate(&mut self) {
+        for x in 0..self.length {
+            for z in 0..self.length {
+                let tall = self
+                    .noise
+                    .get([x as f64 / self.scale, z as f64 / self.scale]);
+                let terrain_height = (tall * self.amp as f64 + (self.height / 2) as f64) as i32;
+
+                for y in 0..self.height {
+                    if y < terrain_height as usize {
+                        self.set(x, y, z, BlockType::Stone);
+                    } else if y == terrain_height as usize {
+                        self.set(x, y, z, BlockType::Grass);
+                    } else {
+                    }
+                }
+            }
+        }
+    }
+
+    fn get(&self, x: usize, y: usize, z: usize) -> Option<&BlockType> {
+        self.blocks.get((x, y, z))
+    }
+
+    fn set(&mut self, x: usize, y: usize, z: usize, block: BlockType) {
+        if let Some(cell) = self.blocks.get_mut((x, y, z)) {
+            *cell = block;
+        }
+    }
+}
+
+pub fn generate(mut query: Query<&mut Chunk, Without<ChunkRendered>>) {
+    for mut chunk in query.iter_mut() {
+        chunk.generate();
+    }
+}
+
+pub fn render_chunk(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+    query: Query<(Entity, &Chunk), Without<ChunkRendered>>,
+) {
+    for (entity, chunk) in query {
+        for ((x, y, z), &block) in chunk.blocks.indexed_iter() {
+            if block == BlockType::Grass {
+                commands.spawn((
+                    Mesh3d(meshes.add(Cuboid::default())),
+                    MeshMaterial3d(materials.add(Color::srgb_u8(0, 255, 0))),
+                    Transform::from_xyz(x as f32, y as f32, z as f32),
+                ));
+            }
+            if block == BlockType::Stone {
+                commands.spawn((
+                    Mesh3d(meshes.add(Cuboid::default())),
+                    MeshMaterial3d(materials.add(Color::srgb_u8(115, 110, 115))),
+                    Transform::from_xyz(x as f32, y as f32, z as f32),
+                ));
+            }
+            println!("{:?}", block);
+        }
+        commands.entity(entity).insert(ChunkRendered);
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -2,71 +2,12 @@ use bevy::{
     camera_controller::free_camera::FreeCameraPlugin,
     dev_tools::fps_overlay::{FpsOverlayConfig, FpsOverlayPlugin},
     prelude::*,
-    state::commands,
 };
-use ndarray::Array3;
-use noise::{NoiseFn, Perlin};
 
+use crate::chunk::{Chunk, ChunkGenerated};
+
+mod chunk;
 mod freecam;
-
-#[derive(Clone, Copy, PartialEq, Debug)]
-enum BlockType {
-    Air,
-    Grass,
-    Stone,
-}
-
-struct Chunk {
-    length: usize,
-    height: usize,
-    blocks: Array3<BlockType>,
-    scale: f64, // Noise scale
-    amp: f64,   // Noise amplitude
-    noise: Perlin,
-}
-
-impl Chunk {
-    fn new(length: usize, height: usize, seed: u32) -> Self {
-        Chunk {
-            blocks: Array3::from_elem((length, height, length), BlockType::Air),
-            scale: 32.,
-            amp: 32.,
-            noise: Perlin::new(seed),
-            length,
-            height,
-        }
-    }
-
-    fn generate(&mut self) {
-        for x in 0..self.length {
-            for z in 0..self.length {
-                let tall = self
-                    .noise
-                    .get([x as f64 / self.scale, z as f64 / self.scale]);
-                let terrain_height = (tall * self.amp as f64 + (self.height / 2) as f64) as i32;
-
-                for y in 0..self.height {
-                    if y < terrain_height as usize {
-                        self.set(x, y, z, BlockType::Stone);
-                    } else if y == terrain_height as usize {
-                        self.set(x, y, z, BlockType::Grass);
-                    } else {
-                    }
-                }
-            }
-        }
-    }
-
-    fn get(&self, x: usize, y: usize, z: usize) -> Option<&BlockType> {
-        self.blocks.get((x, y, z))
-    }
-
-    fn set(&mut self, x: usize, y: usize, z: usize, block: BlockType) {
-        if let Some(cell) = self.blocks.get_mut((x, y, z)) {
-            *cell = block;
-        }
-    }
-}
 
 fn main() {
     App::new()
@@ -98,16 +39,19 @@ fn main() {
         })
         .add_plugins(FreeCameraPlugin)
         .add_systems(Startup, scene)
-        .add_systems(Update, overlay_config)
+        .add_systems(
+            Update,
+            (
+                overlay_config,
+                chunk::generate.run_if(resource_exists::<ChunkGenerated>),
+                chunk::render_chunk.run_if(resource_exists::<ChunkGenerated>),
+            ),
+        )
         .add_plugins(freecam::FreeCam)
         .run();
 }
 
-fn scene(
-    mut commands: Commands,
-    mut meshes: ResMut<Assets<Mesh>>,
-    mut materials: ResMut<Assets<StandardMaterial>>,
-) {
+fn scene(mut commands: Commands) {
     commands.spawn((
         DirectionalLight {
             illuminance: light_consts::lux::AMBIENT_DAYLIGHT,
@@ -122,34 +66,8 @@ fn scene(
             -std::f32::consts::FRAC_PI_4,
         )),
     ));
-    let mut chunk = Chunk::new(16, 64, 42);
-    chunk.generate();
-    draw_chunk(commands, meshes, materials, &chunk);
-}
-
-fn draw_chunk(
-    mut commands: Commands,
-    mut meshes: ResMut<Assets<Mesh>>,
-    mut materials: ResMut<Assets<StandardMaterial>>,
-    chunk: &Chunk,
-) {
-    for ((x, y, z), &block) in chunk.blocks.indexed_iter() {
-        if block == BlockType::Grass {
-            commands.spawn((
-                Mesh3d(meshes.add(Cuboid::default())),
-                MeshMaterial3d(materials.add(Color::srgb_u8(0, 255, 0))),
-                Transform::from_xyz(x as f32, y as f32, z as f32),
-            ));
-        }
-        if block == BlockType::Stone {
-            commands.spawn((
-                Mesh3d(meshes.add(Cuboid::default())),
-                MeshMaterial3d(materials.add(Color::srgb_u8(115, 110, 115))),
-                Transform::from_xyz(x as f32, y as f32, z as f32),
-            ));
-        }
-        println!("{:?}", block);
-    }
+    commands.spawn(Chunk::new(16, 64, 42));
+    commands.spawn(ChunkGenerated);
 }
 
 fn overlay_config(input: Res<ButtonInput<KeyCode>>, mut overlay: ResMut<FpsOverlayConfig>) {
